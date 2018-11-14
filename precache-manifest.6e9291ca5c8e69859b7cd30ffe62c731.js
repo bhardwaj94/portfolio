a593296d3bc9bb728cd75c86eaa130c0b4f967a7
bhardwaj94 <bhardwaj.info2cs@gmail.com>
2018-11-14 15:04:19 +0530

    Updates

---
+++ precache-manifest.6e9291ca5c8e69859b7cd30ffe62c731.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "2ea7e8450e11ce293345",
+    "url": "/portfolio/static/js/main.2ea7e845.chunk.js"
+  },
+  {
+    "revision": "c888fef120d23c05bcae",
+    "url": "/portfolio/static/js/1.c888fef1.chunk.js"
+  },
+  {
+    "revision": "2ea7e8450e11ce293345",
+    "url": "/portfolio/static/css/main.c6f71895.chunk.css"
+  },
+  {
+    "revision": "85acc65df0f245571afd695d421c9331",
+    "url": "/portfolio/index.html"
+  }
+];
